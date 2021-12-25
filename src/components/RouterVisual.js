96da455e9478954e6143cba80869b743d53b9ef5
XuanZhu <51904258+Nagisa-Kaworu@users.noreply.github.com>
2021-12-25 23:20:40 +0800

    添加了可视化模块

--- src/components/RouterVisual.js
+++ src/components/RouterVisual.js
@@ -1,17 +1,36 @@
-import { Button } from '@douyinfe/semi-ui'
+import { Button, Modal, Toast,Card,  Col, Row} from '@douyinfe/semi-ui'
 import React from 'react'
+import NetworkApi from '../services/api'
+import Picture from './Picture';
+import Test from './test'
 const RouterVisual = ({ devIds, setSelectedDev }) => {
 
   const onClickConnectRouter = async (routerId) => {
     setSelectedDev(routerId)
+    const result = await NetworkApi.connectRouter(routerId)
+    const opts = {
+      content: JSON.stringify(result),
+      duration: 3,
+    }
+    Toast.info(opts)
   }
+  
   return (
     <div>
-      拓扑可视化模块
-      <Button onClick={() => onClickConnectRouter(0)}>Router1</Button>
-      <Button onClick={() => onClickConnectRouter(1)}>Router2</Button>
-      <Button onClick={() => onClickConnectRouter(2)}>Router3</Button>
+      <Row>
+        拓扑可视化模块
+        <Button onClick={() => onClickConnectRouter(0)}>Router1</Button>
+        <Button onClick={() => onClickConnectRouter(1)}>Router2</Button>
+        <Button onClick={() => onClickConnectRouter(2)}>Router3</Button>
+      </Row>
+      <br/>
+      <br/>
+      <br/>
+      <Row>
+          <Card><Picture /></Card>
+      </Row>
     </div>
+
   )
 }
 
